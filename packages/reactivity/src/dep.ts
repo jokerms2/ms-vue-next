2a09209d8ef4dfe21d71027df2c3d83810faf4f6
mashuai <mashuai@vomont.com>
2021-08-12 16:36:39 +0800

    add: dep.ts effect.ts effectScope.ts operations.ts warning.ts

---
+++ packages/reactivity/src/dep.ts
@@ -0,0 +1,19 @@
+import { ReactiveEffect, trackOpBit } from "./effect";
+
+export type Dep = Set<ReactiveEffect> & TrackedMarkers
+
+type TrackedMarkers = {
+  w: number,
+  n: number
+}
+
+export const createDep = (effects?: ReactiveEffect[]): Dep => {
+  const dep = new Set<ReactiveEffect>(effects) as Dep
+  dep.w = 0
+  dep.n = 0
+  return dep
+}
+
+export const wasTracked = (dep: Dep): boolean => (dep.w && trackOpBit) > 0
+
+export const newTracked = (dep: Dep): boolean => (dep.n && trackOpBit) > 0
---
+++ packages/reactivity/src/effect.ts
@@ -0,0 +1,217 @@
+import { TrackOpTypes, TriggerOpTypes } from "./operations";
+import { createDep, Dep, wasTracked, newTracked } from './dep'
+import { extend } from '@vue/shared'
+import { EffectScope, recordEffectScope} from "./effectScope";
+
+type KeyToDepMap = Map<any, Dep>
+const targetMap = new WeakMap<any, KeyToDepMap>()
+
+let effectTrackDepth = 0
+
+export let trackOpBit = 1
+
+const maxMarkerBits = 30
+
+export type DebuggerEvent = {
+  effect: ReactiveEffect
+} & DebuggerEventExtraInfo
+
+export type EffectScheduler = (...args: any[]) => any
+
+export type DebuggerEventExtraInfo = {
+  target: object
+  type: TrackOpTypes | TriggerOpTypes
+  key: any
+  newValue?: any
+  oldValue?: any
+  oldTarget?: Map<any, any> | Set<any>
+}
+
+const effectStack: ReactiveEffect[] = []
+let activeEffect: ReactiveEffect | undefined
+
+export class ReactiveEffect<T = any> {
+  active = true
+  deps: Dep[] = []
+
+  computed?: boolean
+  allowRecurse?: boolean
+  onStop?: () => void
+  onTrack?: (event: DebuggerEvent) => void
+
+  constructor(
+    public fn: () => T,
+    public scheduler: EffectScheduler | null = null,
+    scope?: EffectScope | null
+  ) {
+    recordEffectScope(this, scope)
+  }
+
+  run () {
+    if (!this.active) {
+      return this.fn()
+    }
+    if (!effectStack.includes(this)) {
+      try {
+        effectStack.push((activeEffect = this))
+
+        enableTracking()
+
+        trackOpBit = 1 << ++effectTrackDepth
+
+        if (effectTrackDepth <= maxMarkerBits) {
+          
+        } else {
+          cleanupEffect(this)
+        }
+        return this.fn()
+      } finally {
+        if (effectTrackDepth <= maxMarkerBits) {
+          
+        }
+
+        trackOpBit = 1 << --effectTrackDepth
+
+        resetTracking()
+        effectStack.pop()
+        const n = effectStack.length
+        activeEffect = n > 0 ? effectStack[n - 1]: undefined
+      }
+    }
+  }
+
+  stop () {
+    if (this.active) {
+      cleanupEffect(this)
+      if (this.onStop) {
+        this.onStop()
+      }
+      this.active = false
+    }
+  }
+}
+
+function cleanupEffect (effect: ReactiveEffect) {
+  const { deps } = effect
+  if (deps.length) {
+    for (let i = 0; i < deps.length; i++) {
+      deps[i].delete(effect)
+    }
+    deps.length = 0
+  }
+}
+
+export interface DebuggerOptions {
+  onTrack?: (event: DebuggerEvent) => void
+  onTrigger?: (event: DebuggerEvent) => void
+}
+
+export interface ReactiveEffectOptions extends DebuggerOptions {
+  lazy?: boolean
+  scheduler?: EffectScheduler
+  scope?: EffectScope
+  allowRecurse?: boolean
+  onStop: () => void
+}
+
+export interface ReactiveEffectRunner<T = any> {
+  (): T
+  effect: ReactiveEffect
+}
+
+export function effect<T = any>(
+  fn: () => T,
+  options?: ReactiveEffectOptions
+) {
+  if ((fn as ReactiveEffectRunner).effect) {
+    fn = (fn as ReactiveEffectRunner).effect.fn
+  }
+
+  const _effect = new ReactiveEffect(fn)
+  if (options) {
+    extend(_effect, options)
+    if (options.scope) recordEffectScope(_effect, options.scope)
+  }
+  if (!options || !options.lazy) {
+    _effect.run()
+  }
+  const runner = _effect.run.bind(_effect) as ReactiveEffectRunner
+  runner.effect = _effect
+  return runner
+}
+
+export function stop(runner: ReactiveEffectRunner) {
+  runner.effect.stop()
+}
+
+let shouldTrack = true
+const trackStack: boolean[] = []
+
+export function pauseTracking() {
+  trackStack.push(shouldTrack)
+  shouldTrack = false
+}
+
+export function enableTracking() {
+  trackStack.push(shouldTrack)
+  shouldTrack = true
+}
+
+export function resetTracking() {
+  const last = trackStack.pop()
+  shouldTrack = last === undefined ? true : last
+}
+
+export function track(target: object, type: TrackOpTypes, key: unknown) {
+  if (!isTracking) {
+    return
+  }
+  let depsMap = targetMap.get(target)
+  if (!depsMap) {
+    targetMap.set(target, (depsMap = new Map()))
+  }
+  let dep = depsMap.get(key)
+  if (!dep) {
+    depsMap.set(key, (dep = createDep()))
+  }
+
+  const eventInfo = __DEV__
+    ? { effect: activeEffect, target, type, key}
+    : undefined
+  
+    trackEffects(dep, eventInfo)
+}
+
+export function isTracking() {
+  return shouldTrack && activeEffect !== undefined
+}
+
+export function trackEffects(
+  dep: Dep,
+  debuggerEventExtraInfo?: DebuggerEventExtraInfo
+) {
+  let shouldTrack = false
+  if (effectTrackDepth <= maxMarkerBits) {
+    if (!newTracked(dep)) {
+      dep.n |= trackOpBit
+      shouldTrack = !wasTracked(dep)
+    }
+  } else {
+    shouldTrack = !dep.has(activeEffect!)
+  }
+
+  if (shouldTrack) {
+    dep.add(activeEffect!)
+    activeEffect!.deps.push(dep)
+    if (__DEV__ && activeEffect!.onTrack) {
+      activeEffect!.onTrack(
+        Object.assign(
+          {
+            effect: activeEffect!
+          },
+          debuggerEventExtraInfo
+        )
+      )
+    }
+  }
+}
